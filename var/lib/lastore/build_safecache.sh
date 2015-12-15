026f97c76975b8fc46c6deed0429a899b9f98534
snyh <snyh@snyh.org>
2015-12-15 14:07:55 +0800

    Improve build_safecache.sh for delete old data
    Change-Id: Ib5d9d22b257ae7d14628bf253999be023d0d2a7a

--- var/lib/lastore/build_safecache.sh
+++ var/lib/lastore/build_safecache.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p /var/lib/lastore/safecache/lists
-cp -vu /var/lib/apt/lists/* /var/lib/lastore/safecache/lists
+source=/var/lib/apt/lists/
+target=/var/lib/lastore/safecache/lists/
+
+mkdir -p ${target}
+
+# clean old list files
+cd $target
+find . -type f |
+while read fname
+do
+    #echo "Check... ${fname}"
+    if [[ ! -f ${source}/${fname} ]]; then
+	echo "remove old file: ${target}${fname}"
+	rm -rf ${target}${fname}
+    fi
+done
+
+# update list files
+cp -vu ${source}/* $target
 cp -vu /var/cache/apt/pkgcache.bin /var/lib/lastore/safecache/
-apt-cache gencaches -c /var/lib/lastore/apt.conf
