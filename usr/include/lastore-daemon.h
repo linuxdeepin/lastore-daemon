2a9cc57293861c484d2244406462eb8320730245
snyh <snyh@snyh.org>
2016-12-06 14:19:45 +0800

    Support QT_NO_KEYWORDS
    Change-Id: Iaaa2d7a010a4346a8cca68925dbf83f5e2e9ec8b

--- usr/include/lastore-daemon.h
+++ usr/include/lastore-daemon.h
@@ -216,7 +216,7 @@ void DBusObject::propertyChanged(const QDBusMessage& msg)
         return;
 
     QVariantMap changedProps = qdbus_cast<QVariantMap>(arguments.at(1).value<QDBusArgument>());
-    foreach(const QString &prop, changedProps.keys()) {
+    Q_FOREACH(const QString &prop, changedProps.keys()) {
         const QMetaObject* self = metaObject();
         for (int i=self->propertyOffset(); i < self->propertyCount(); ++i) {
             QMetaProperty p = self->property(i);
