1028f5c44b90cf9aa19c5ea0712b0b1fecc91468
snyh <snyh@snyh.org>
2018-02-12 17:17:15 +0800

    perf: reduce RAM usage by avoid contents of safecache
    1. change pkgcache.bin as a hard link
    2. remove apt source lists restrict.
    
    Change-Id: I10e8d87b5e6253597c501e08e9c701f534c1ff8e

--- var/lib/lastore/scripts/build_safecache.sh
+++ var/lib/lastore/scripts/build_safecache.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-
-source=/var/lib/apt/lists/
-target=/var/lib/lastore/safecache/lists/
-
-mkdir -p ${target}
-
-# clean old list files
-cd $target
-find . -type f |
-while read fname
-do
-    #echo "Check... ${fname}"
-    if [[ ! -f ${source}/${fname} ]]; then
-	echo "remove old file: ${target}${fname}"
-	rm -rf ${target}${fname}
-    fi
-done
-
-# update list files
-find ${source} -maxdepth 1 -type f -exec cp '{}' ${target} \;
-cp  /var/cache/apt/pkgcache.bin /var/lib/lastore/safecache/
+# /var/cache/apt/pkgcache.bin will be deleted temporarily
+# whenever executing "apt-get update".
+ln -fv /var/cache/apt/pkgcache.bin /var/lib/lastore/safecache/pkgcache.bin
