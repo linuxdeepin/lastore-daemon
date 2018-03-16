e7d91ebb520a00801d747d23d5371c6b4166ca2d
snyh <snyh@snyh.org>
2018-03-16 09:47:35 +0800

    fix: failed create hardlink if the parenter directory miss
    Change-Id: I3353528aa9aaec8cb42b9a4ab779fe2da9a4a232

--- var/lib/lastore/scripts/build_safecache.sh
+++ var/lib/lastore/scripts/build_safecache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # /var/cache/apt/pkgcache.bin will be deleted temporarily
 # whenever executing "apt-get update".
-ln -fv /var/cache/apt/pkgcache.bin /var/lib/lastore/safecache/pkgcache.bin
+ln -fv /var/cache/apt/pkgcache.bin /var/lib/lastore/safecache_pkgcache.bin
