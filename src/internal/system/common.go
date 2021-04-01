ee312a236366f839930060417084bc920080f43e
lichangze <lichangze@uniontech.com>
2021-04-01 17:10:49 +0800

    fix: 修复控制中心以及lastore-tools被更新后,检查更新失败的问题
    由于控制中心更新,导致获取调用者可执行路径出错,修改获取可执行路径的方式;
    lastore-daemon更新之后,lastore-daemon进程未重启,所以依旧是旧版本的daemon,但是lastore-tools为新版本,针对旧版本配置做相应兼容
    
    Log: 修复控制中心以及lastore-tools被更新后,检查更新失败的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-69474.html
    Change-Id: Icb181d7e02752ebc6ca461b45d17116ab1fabafb

--- src/internal/system/common.go
+++ src/internal/system/common.go
@@ -20,8 +20,13 @@ package system
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
+
+	log "github.com/cihub/seelog"
 )
 
 type MirrorSource struct {
@@ -91,4 +96,82 @@ func SystemUpgradeInfo() ([]UpgradeInfo, error) {
 		return nil, fmt.Errorf("Invalid update_infos: %v\n", err)
 	}
 	return r, nil
+}
+
+// 用于设置UpdateMode属性,最大支持64位
+const (
+	SystemUpdate   = 1 << 0 // 系统更新
+	AppStoreUpdate = 1 << 1 // 应用更新
+	SecurityUpdate = 1 << 2 // 安全更新
+)
+
+func UpdateCustomSourceDir(mode uint64) error {
+	const (
+		lastoreSourcesPath = "/var/lib/lastore/sources.list"
+		customSourceDir    = "/var/lib/lastore/sources.list.d"
+		sourceListPath     = "/etc/apt/sources.list"
+		originSourceDir    = "/etc/apt/sources.list.d"
+	)
+
+	// 移除旧的sources.list.d内容,再根据最新配置重新填充
+	err := os.RemoveAll(customSourceDir)
+	if err != nil {
+		_ = log.Warn(err)
+	}
+	err = os.MkdirAll(customSourceDir, 0755)
+	if err != nil {
+		_ = log.Warn(err)
+	}
+
+	// 移除旧的sources.list,再根据最新配置重新创建链接
+	err = os.Remove(lastoreSourcesPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			_ = log.Warn(err)
+		}
+	}
+
+	var customSourceFilePaths []string
+	if mode&SystemUpdate == SystemUpdate {
+		customSourceFilePaths = append(customSourceFilePaths, sourceListPath)
+	}
+	sourceDirFileInfos, err := ioutil.ReadDir(originSourceDir)
+	if err != nil {
+		_ = log.Warn(err)
+	}
+	for _, fileInfo := range sourceDirFileInfos {
+		name := fileInfo.Name()
+		if strings.HasSuffix(name, ".list") {
+			switch name {
+			case "appstore.list":
+				if mode&AppStoreUpdate == AppStoreUpdate {
+					customSourceFilePaths = append(customSourceFilePaths, filepath.Join(originSourceDir, name))
+				}
+			case "safe.list":
+				if mode&SecurityUpdate == SecurityUpdate {
+					customSourceFilePaths = append(customSourceFilePaths, filepath.Join(originSourceDir, name))
+				}
+			default:
+				if mode&SystemUpdate == SystemUpdate {
+					customSourceFilePaths = append(customSourceFilePaths, filepath.Join(originSourceDir, name))
+				}
+			}
+		}
+	}
+
+	// 创建对应的软链接
+	for _, customFilePath := range customSourceFilePaths {
+		var customFileLinkPath string
+		if customFilePath == sourceListPath {
+			customFileLinkPath = lastoreSourcesPath
+		} else {
+			customFileLinkPath = filepath.Join(customSourceDir, filepath.Base(customFilePath))
+		}
+
+		err = os.Symlink(customFilePath, customFileLinkPath)
+		if err != nil {
+			return fmt.Errorf("create symlink for %q failed: %v", customFileLinkPath, err)
+		}
+	}
+	return nil
 }
