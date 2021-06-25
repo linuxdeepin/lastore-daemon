6f4afb7a0ab4531d17a338024302741e88e0bff2
tanfeng <tanfeng@uniontech.com>
2021-06-25 17:28:55 +0800

    test: 替换部分单元测试的框架为testify
    job、jobmanager、config、system等几个模块的单元测试的框架替换为testify
    
    Log: 统一了单元测试框架为testify，替换掉非testify框架的测试用例
    Change-Id: Id6a0b95f9cf772fd0555e96f7387780124afc51d

--- src/lastore-smartmirror-daemon/config_test.go
+++ src/lastore-smartmirror-daemon/config_test.go
@@ -1,51 +1,38 @@
 package main
 
 import (
-	"internal/system"
 	"io/ioutil"
 	"os"
-	"path"
 	"testing"
 
-	C "gopkg.in/check.v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-type configSuite struct{}
+func TestConfig(t *testing.T) {
+	testDataPath := "./TemporaryTestDataDirectoryNeedDelete"
+	err := os.Mkdir(testDataPath, 0777)
+	require.Nil(t, err)
+	defer func() {
+		err := os.RemoveAll(testDataPath)
+		require.Nil(t, err)
+	}()
+	tmpfile, err := ioutil.TempFile(testDataPath, "config.json")
+	require.Nil(t, err)
+	defer tmpfile.Close()
 
-func TestConfig(t *testing.T) { C.TestingT(t) }
-func init() {
-	C.Suite(&configSuite{})
-}
-
-func (*configSuite) TestSuiteConfig(c *C.C) {
-	// Suite临时文件测试config,Suite结束后会自动销毁c.MkDir()创建的目录
-	dir := c.MkDir()
-	tmpfile, err := ioutil.TempFile(dir, "config.json")
-	c.Assert(err, C.Equals, nil)
-
-	data, err := ioutil.ReadFile(path.Join(system.VarLibDir, configDataFilepath))
-	if err != nil {
-		if os.IsNotExist(err) {
-			c.Log("TestSuiteConfig ReadFile error:", err)
-			data = []byte("{\"filePath\":\"/\",\"Enable\":true}")
-		} else {
-			c.Assert(err, C.Equals, nil)
-		}
-	}
-
+	data := []byte(`{"filePath":"/","Enable":true}`)
 	err = ioutil.WriteFile(tmpfile.Name(), data, 0777)
-	c.Assert(err, C.Equals, nil)
-
+	require.Nil(t, err)
 	configBefore := newConfig(tmpfile.Name())
-	c.Assert(configBefore, C.NotNil)
+	require.NotNil(t, configBefore)
 	config := newConfig(tmpfile.Name())
-	c.Assert(config, C.NotNil)
-
+	require.NotNil(t, config)
 	err = config.setEnable(!config.Enable)
-	c.Assert(err, C.Equals, nil)
+	require.Nil(t, err)
 
 	// 验证
 	configAfter := newConfig(tmpfile.Name())
-	c.Assert(configAfter, C.NotNil)
-	c.Check(configAfter.Enable, C.Equals, !configBefore.Enable)
+	require.NotNil(t, configAfter)
+	assert.Equal(t, configAfter.Enable, !configBefore.Enable)
 }
