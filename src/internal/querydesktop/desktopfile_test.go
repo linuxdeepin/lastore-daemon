a6dadf4c2070d1006499569313c9609285ad50b5
lichangze <lichangze@uniontech.com>
2021-09-17 13:48:49 +0800

    test: 增加internal/querydesktop模块单元测试
    增加internal/querydesktop模块单元测试
    
    Log: 增加internal/querydesktop模块单元测试
    Task: https://pms.uniontech.com/zentao/task-view-79863.html
    Change-Id: Ie0dbdb903113785f17f6d996c957516704c27de4

--- src/internal/querydesktop/desktopfile_test.go
+++ src/internal/querydesktop/desktopfile_test.go
@@ -21,6 +21,8 @@ import (
 	"internal/utils"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -63,4 +65,64 @@ func ListInstalled(t *testing.T) []string { // nolint
 		t.Fatal(err)
 	}
 	return strings.Split(s, "\n")
+}
+
+func Test_Len(t *testing.T) {
+	dFile := DesktopFiles{
+		PkgName: "",
+		Files: []string{
+			"0.desktop",
+			"1.desktop",
+		},
+	}
+	assert.Equal(t, 2, dFile.Len())
+}
+
+func Test_Swap(t *testing.T) {
+	dFile := DesktopFiles{
+		PkgName: "",
+		Files: []string{
+			"0.desktop",
+			"1.desktop",
+		},
+	}
+	dFile.Swap(0, 1)
+	assert.Equal(t, "1.desktop", dFile.Files[0])
+	assert.Equal(t, "0.desktop", dFile.Files[1])
+}
+
+func Test_score(t *testing.T) {
+	dFile := DesktopFiles{
+		PkgName: "testdata",
+		Files: []string{
+			"./testdata/electron-ssr.desktop",
+			"./testdata/google-chrome.desktop",
+			"./testdata/isomaster.desktop",
+		},
+	}
+	assert.Equal(t, 29, dFile.score(0))
+	assert.Equal(t, 34, dFile.score(1))
+	assert.Equal(t, 34, dFile.score(2))
+}
+
+func Test_BestOne(t *testing.T) {
+	dFile := DesktopFiles{
+		PkgName: "testdata",
+		Files: []string{
+			"./testdata/electron-ssr.desktop",
+			"./testdata/google-chrome.desktop",
+		},
+	}
+	assert.Equal(t, "./testdata/google-chrome.desktop", dFile.BestOne())
+}
+
+func Test_Less(t *testing.T) {
+	dFile := DesktopFiles{
+		PkgName: "testdata",
+		Files: []string{
+			"./testdata/electron-ssr.desktop",
+			"./testdata/google-chrome.desktop",
+		},
+	}
+	assert.True(t, dFile.Less(0, 1))
 }
