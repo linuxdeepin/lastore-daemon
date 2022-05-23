062b9004f32ef04c0fc7b6302e40f7335a9d22d8
weizhixiang <weizhixiang@uniontech.com>
2022-05-23 10:34:21 +0800

    feat: 允许dde-application-manager项目卸载应用
    在允许调用RemovePackage接口的列表中，增加dde-applicaiton-manager
    
    Log: 允许dde-application-manager项目卸载应用
    Task: https://pms.uniontech.com/task-view-133765.html
    Influence: 允许dde-appliation-manager卸载应用
    Change-Id: Ic3adf60fab90bae408b53af03edfcf10f76712e2

--- src/lastore-daemon/manager.go
+++ src/lastore-daemon/manager.go
@@ -58,6 +58,7 @@ const (
 	langSelectorPath      = "/usr/lib/deepin-daemon/langselector"
 	controlCenterPath     = "/usr/bin/dde-control-center"
 	controlCenterCmdLine  = "/usr/share/applications/dde-control-center.deskto" // 缺个 p 是因为 deepin-turbo 修改命令的时候 buffer 不够用, 所以截断了.
+	amDaemonPath          = "/usr/bin/deepin-application-manager"
 )
 
 var (
@@ -75,6 +76,7 @@ var (
 		sessionDaemonPath,
 		langSelectorPath,
 		controlCenterPath,
+		amDaemonPath,
 	}
 )
 
