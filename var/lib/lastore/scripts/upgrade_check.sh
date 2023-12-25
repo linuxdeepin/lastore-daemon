b8a622f17b5a20e62205ca09acd282d33e0da92a
lichangze <lichangze@uniontech.com>
2023-12-25 10:04:27 +0800

    chore: 移除lastore-daemon自启动
    1e12dc3c提交增加自启动，为了解决更新安装后重启检查前端无法成功调用lastore-daemon接口问题；现在修改为upgrade_check.sh服务启动lastore-daemon服务
    
    Log: 移除lastore-daemon自启动
    Influence: 更新后重启检查
    Change-Id: Ibcbfcf7a1ee092cdb905d3ab0bf875a721fd9ec8

--- var/lib/lastore/scripts/upgrade_check.sh
+++ var/lib/lastore/scripts/upgrade_check.sh
@@ -2,6 +2,7 @@
 if [ -e "/etc/deepin/deepin_update_option.json" ] && [ ! -L "/tmp/deepin_update_option.json" ];  then
     # 如果文件存在，则创建软连接
     ln -s "/etc/deepin/deepin_update_option.json" "/tmp/deepin_update_option.json"
+    systemctl start lastore-daemon.service > /dev/null || true
 else
     echo "deepin_update_option.json not exist"
 fi
