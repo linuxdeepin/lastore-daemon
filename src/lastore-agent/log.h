3d7d81ae9873484c090c9f65a7e276333621df3c
electricface <songwentai@uniontech.com>
2025-12-05 10:55:06 +0800

    refactor(agent): Avoid compilation warnings
    * Remove redundant header file inclusions to reduce compilation
    dependencies
    * Unify the use of the AGENT_UNUSED macro to mark unused parameters
    * Modify the main function signature to a parameterless form

--- src/lastore-agent/log.h
+++ src/lastore-agent/log.h
@@ -5,9 +5,8 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-#include <stdio.h>
 #include <syslog.h>
 
-#define LOG(level, format, ...)                                                \
-  syslog(level, "%s:%d " format "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG(level, format, ...) \
+    syslog(level, "%s:%d " format "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
