86966affbcecdaefc28929cf77f4196865350f71
xml <xiamengliang@uniontech.com>
2026-01-29 10:03:43 +0800

    fix: Remove changes related to P2P
    Task: https://pms.uniontech.com/task-view-385361.html

--- src/internal/updateplatform/message_report.go
+++ src/internal/updateplatform/message_report.go
@@ -772,15 +772,7 @@ func (m *UpdatePlatformManager) genUpdatePolicyByToken(updateInRelease bool) err
 	m.targetBaseline = msg.Version.Baseline
 	m.targetVersion = msg.Version.Version
 	m.systemTypeFromPlatform = msg.SystemType
-	for _, repo := range msg.RepoInfos {
-		m.repoInfos = append(m.repoInfos, repoInfo{
-			Cdn:      strings.ReplaceAll(repo.Cdn, "p2p://", "https://"),
-			CodeName: repo.CodeName,
-			Source:   strings.ReplaceAll(repo.Source, "p2p://", "https://"),
-			Uri:      strings.ReplaceAll(repo.Uri, "p2p://", "https://"),
-			Version:  repo.Version,
-		})
-	}
+	m.repoInfos = msg.RepoInfos
 	m.taskID = msg.Version.TaskID
 	logger.Infof("current policy task id: %v", m.taskID)
 	m.checkTime = time.Now().String()
